76e35f8ebf251451bd05546a274d3e2c3b48a21d
MariKerdika <131894818+MariKerdika@users.noreply.github.com>
2023-10-13 18:09:49 +0400

    fetch data part 1

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,21 @@
-import { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import SideBar from "./SideBar";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import Videos from "./Videos";
+import Sidebar from "./SideBar";
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState(null);
+
+  useEffect(() => {
+    setVideos(null);
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box
@@ -12,19 +25,31 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <SideBar />
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+
         <Typography
           className="copyright"
           variant="body2"
-          sx={{
-            mt: 1.5,
-            color: "#fff",
-            borderTop: "1px solid grey",
-            paddingTop: "5px",
-          }}
+          sx={{ mt: 1.5, color: "#fff" }}
         >
-          Copyright 2022
+          Copyright © 2022 JSM Media
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
+        </Typography>
+
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
